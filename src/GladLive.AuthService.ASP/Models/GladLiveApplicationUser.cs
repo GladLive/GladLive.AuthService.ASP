b279971af37b97db8ff93d4d0321a5f47606a09b
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-09 04:35:53 -0500

    Switched to JWT Authentication

---
+++ src/GladLive.AuthService.ASP/Models/GladLiveApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using OpenIddict;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladLive.AuthService.ASP
+{
+	/// <summary>
+	/// GladLive OpenIddict app user.
+	/// See Documentation for details: https://github.com/openiddict/openiddict-core
+	/// </summary>
+	public class GladLiveApplicationUser : OpenIddictUser
+	{
+
+	}
+}
